7ef9be3be9d3d67c337733e3616692352ec89757
SyaifudinAmin <35301610+SyaifudinAmin@users.noreply.github.com>
2018-03-13 18:29:46 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>Login</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-widht,initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="asset/css/Style.css">
+        
+    </head>
+    <body>
+        <div class="container">
+            <img src="asset/img/banner_login.png">
+            
+            <form>
+                
+                <div class="form-input">
+                    <input type="text" name="username" placeholder="Email Address" autofocus="yes" required="yes">
+                </div>   
+                <div class="form-input">
+                    <input type="password" name="password" placeholder="Password" autofocus="yes" required="yes">
+                </div>
+                
+                <input type="submit" name="submit" value="LOGIN" class="btn-login">
+                
+                    
+            </form><br>
+            <a href="$"> Forget password?</a>
+        </div>
+        
+        <footer>
+            <p>login form<a class="footer-text" href="$">Syaifudin </a>
+        </footer>
+    </body>
+</html>
